cd14d7b6497fbcfc12d8e978f78c3b19650dec42
庄宏基 <zhuanghongji.com@gmail.com>
2023-11-02 11:56:26 +0800

    feat: add Example project

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by zhuanghongji on 2023/11/2.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
