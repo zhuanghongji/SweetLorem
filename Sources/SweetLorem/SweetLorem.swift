d06bb69063283cc94e931596eced1b2b77d4e8c5
庄宏基 <zhuanghongji.com@gmail.com>
2023-11-02 12:12:41 +0800

    feat: add SweetLorem as a package dependency of Example

--- Sources/SweetLorem/SweetLorem.swift
+++ Sources/SweetLorem/SweetLorem.swift
@@ -1,2 +1,8 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public class SweetLorem {
+    public static func name() -> String {
+        return "SweetLorem"
+    }
+}
